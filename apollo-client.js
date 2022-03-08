9a55fedd7c836690e85896f1b0ea1a94f0512527
Hadi Rasekh <hadi_rasekh@yahoo.com>
2022-03-08 22:18:48 +0330

    fix apollo client

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'http://127.0.0.1:3000/graphql',
+    uri: `${process.env.NEXT_PUBLIC_API_DOMAIN}/graphql`,
     cache: new InMemoryCache,
 });
 
